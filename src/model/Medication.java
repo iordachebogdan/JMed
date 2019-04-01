0462c4eeeee4c7974211b368c87afa5da59fdf9b
Bogdan Iordache <iordache_bogdan1998@yahoo.com>
2019-04-01 21:49:41 +0300

    Make Symptom and Medication immutable

--- src/model/Medication.java
+++ src/model/Medication.java
@@ -3,10 +3,10 @@ package model;
 import java.util.Date;
 
 public class Medication {
-    private String name;
-    private String administration;
-    private Date fromDate;
-    private Date toDate;
+    private final String name;
+    private final String administration;
+    private final Date fromDate;
+    private final Date toDate;
 
     public Medication(String name, String administration, Date fromDate, Date toDate) {
         this.name = name;
@@ -19,31 +19,15 @@ public class Medication {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getAdministration() {
         return administration;
-    }
-
-    public void setAdministration(String administration) {
-        this.administration = administration;
     }
 
     public Date getFromDate() {
         return fromDate;
     }
 
-    public void setFromDate(Date fromDate) {
-        this.fromDate = fromDate;
-    }
-
     public Date getToDate() {
         return toDate;
-    }
-
-    public void setToDate(Date toDate) {
-        this.toDate = toDate;
     }
 }
--- src/model/Symptom.java
+++ src/model/Symptom.java
@@ -3,8 +3,8 @@ package model;
 import java.util.Date;
 
 public class Symptom {
-    private String description;
-    private Date firstAppearance;
+    private final String description;
+    private final Date firstAppearance;
 
     public Symptom(String description, Date firstAppearance) {
         this.description = description;
@@ -15,15 +15,7 @@ public class Symptom {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Date getFirstAppearance() {
         return firstAppearance;
-    }
-
-    public void setFirstAppearance(Date firstAppearance) {
-        this.firstAppearance = firstAppearance;
     }
 }
